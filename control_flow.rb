a70a6ac54beb260d1840bb83a7e24c29c5e1f0dd
Junior <kazunguderiq@gmail.com>
2022-10-11 23:29:29 +0300

    commit tree 1fb22904f6d0923c019015291878dcdbb5a46059

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,23 @@
 def admin_login(username, password)
   # your code here
+  unless ((username == "admin" || username == "ADMIN") && (password == 12345))
+    puts "Access denied"
+  else
+    puts "Access granted"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature > 85
+    "It's too dang hot out there!"
+  elsif ((temperature >= 40) && (temperature <= 60))
+    "It's a little chilly out there!"
+  elsif temperature < 40
+    "It's a brisk out there!"
+  else
+    "It's perfect out there"
+  end
 end
 
 def fizzbuzz(num)
@@ -13,4 +27,3 @@ end
 def calculator(operation, num1, num2)
   # your code here
 end
-
